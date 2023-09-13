9837ea105169f42302dce7ce18e8cd3c5b26b132
Tanya501 <144354026+Tanya501@users.noreply.github.com>
2023-09-13 16:32:46 +1000

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -3,7 +3,7 @@ pipeline {
     stages {
         stage("Build") {
             steps {
-                echo "Buildings ..."
+                echo "Buildings  ..."
             }
             post {
                 always {
